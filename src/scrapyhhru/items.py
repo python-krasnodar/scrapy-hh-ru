0227018c91694defa024a136e5b663669ad456ef
Memory Clutter <memclutter@gmail.com>
2016-07-26 17:19:43 +0300

    Create VacancyItem in items.py

--- src/scrapyhhru/items.py
+++ src/scrapyhhru/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class ScrapyhhruItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class VacancyItem(scrapy.Item):
+    title = scrapy.Field()
+    description = scrapy.Field()
+
