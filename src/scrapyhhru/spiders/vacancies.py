a0cd7a85e1fad128d7480aca60ca5de11a64bf11
Memory Clutter <memclutter@gmail.com>
2016-07-27 12:36:36 +0300

    Support spider arguments

--- src/scrapyhhru/spiders/vacancies.py
+++ src/scrapyhhru/spiders/vacancies.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from urllib.parse import urlencode
 import scrapy
 from scrapyhhru.items import VacancyItem
 from scrapyhhru.settings import HH_RU_MAX_PAGE
@@ -8,6 +9,18 @@ class VacanciesSpider(scrapy.Spider):
     name = "vacancies"
     allowed_domains = ["hh.ru"]
     start_urls = ['https://hh.ru/search/vacancy?page=%s' % page for page in range(0, HH_RU_MAX_PAGE)]
+
+    def __init__(self, name=None, **kwargs):
+        if len(kwargs) != 0:
+            start_url_tpl = 'https://hh.ru/search/vacancy?%s&' % urlencode(kwargs)
+        else:
+            start_url_tpl = 'https://hh.ru/search/vacancy?'
+        start_url_tpl += 'page={0}'
+        self.start_urls = list()
+        for page in range(0, HH_RU_MAX_PAGE):
+            self.start_urls.append(start_url_tpl.format(page))
+
+        super().__init__(name, **kwargs)
 
     def parse(self, response):
         for item in response.css('.search-result-item'):
